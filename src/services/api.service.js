9ba7e22b0b9a16702e24bcb20997bfe6f39fe526
vladegg26 <vladbry@gmail.com>
2024-03-08 02:00:18 +0400

    added api service

---
+++ src/services/api.service.js
@@ -0,0 +1,43 @@
+import md5 from 'md5'
+
+export class ApiService {
+	#API_PASSWORD = 'Valantis'
+
+	#makeRequest(action, params) {
+		const date = new Date()
+		const timestamp = `${date.toISOString().split('T')[0].replaceAll('-', '')}`
+		return fetch('https://api.valantis.store:41000/', {
+			method: 'POST',
+			headers: {
+				'X-Auth': md5(this.#API_PASSWORD + '_' + timestamp),
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				action,
+				params
+			})
+		})
+			.then(res => res.json())
+			.then(res => res.result)
+	}
+	#getByIds(params) {
+		return this.#makeRequest('get_items', params).then(items =>
+			items.reduce((acc, item) => {
+				if (!acc.some(elem => elem.id == item.id)) {
+					acc.push(item)
+				}
+				return acc
+			}, [])
+		)
+	}
+
+	getByFilter(params) {
+		return this.#makeRequest('filter', params).then(itemIds => this.#getByIds({ ids: itemIds }))
+	}
+	getItems(params) {
+		return this.#makeRequest('get_ids', params).then(itemIds => this.#getByIds({ ids: itemIds }))
+	}
+	getItemFields(params) {
+		return this.#makeRequest('get_fields', params)
+	}
+}
