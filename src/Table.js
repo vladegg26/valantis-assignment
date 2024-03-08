9773d6f77dee7bdf59a4c8672e9b718ca9c1bf8b
vladegg26 <vladbry@gmail.com>
2024-03-08 17:47:03 +0400

    added components in table

--- src/Table.js
+++ src/Table.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
-import { Button, Input, Table, Space } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { Button, Table, Select } from 'antd'
 import { ApiService } from './services/api.service'
+import { ArrowLeftOutlined, ArrowRightOutlined } from '@ant-design/icons'
 
 const apiService = new ApiService()
-const data = await apiService.getItems({ offset: 0, limit: 10 })
 const columns = [
 	{
 		title: 'Id',
@@ -11,33 +11,69 @@ const columns = [
 		key: 'id'
 	},
 	{
-		title: 'Product',
+		title: 'Продукт',
 		dataIndex: 'product',
 		key: 'product'
 	},
 	{
-		title: 'Brand',
-
+		title: 'Брэнд',
 		dataIndex: 'brand',
 		key: 'brand'
 	},
 	{
-		title: 'Price',
+		title: 'Цена',
 		dataIndex: 'price',
 		key: 'price'
 	}
 ]
-
-function onPrev() {
-	console.log('prev')
-}
-function onNext() {
-	console.log('next')
-}
+const options = [
+	{
+		value: 10,
+		label: '10'
+	},
+	{
+		value: 30,
+		label: '30'
+	},
+	{
+		value: 50,
+		label: '50'
+	},
+	{
+		value: 100,
+		label: '100'
+	}
+]
 
 function ItemsTable() {
+	const [data, setData] = useState([])
 	const [offset, setOffset] = useState(0)
-	const onNext = () => {}
+	const [limit, setLimit] = useState(10)
+	const [loading, setLoading] = useState(false)
+	const [counter, setCounter] = useState(1)
+
+	const onNext = () => {
+		setOffset(offset + limit)
+		setCounter(counter + 1)
+	}
+	const onPrev = () => {
+		setOffset(offset - limit)
+		setCounter(counter - 1)
+	}
+
+	const changeItemsAmount = value => {
+		setLimit(value)
+	}
+	const fetchData = async () => {
+		setLoading(true)
+		setData(await apiService.getItems({ offset, limit }))
+		setLoading(false)
+	}
+
+	useEffect(() => {
+		fetchData()
+	}, [offset, limit])
+
 	return (
 		<>
 			<div
@@ -47,12 +83,34 @@ function ItemsTable() {
 					flexDirection: 'column'
 				}}
 			>
-				<Table dataSource={data} columns={columns} pagination={false} />
+				<div className='table-wrapper'>
+					<Table dataSource={data} columns={columns} pagination={false} loading={loading} />
+				</div>
 			</div>
-			<div>
-				<Button onClick={onPrev}> Предыдущая страница</Button>
-				<Button onClick={onNext}> Следующая страница</Button>
-			</div>
+
+			<span
+				style={{
+					display: 'flex',
+					justifyContent: 'center'
+				}}
+			>
+				<Button
+					icon={<ArrowLeftOutlined />}
+					disabled={offset - limit < 0}
+					onClick={() => onPrev()}
+					className='button-style'
+				></Button>
+				<span className='counter-style'>{counter} </span>
+				<Button icon={<ArrowRightOutlined />} onClick={() => onNext()} className='button-style'></Button>
+			</span>
+			<span
+				style={{
+					display: 'flex',
+					flexDirection: 'row-reverse'
+				}}
+			>
+				<Select onChange={v => changeItemsAmount(v)} style={{ width: 100 }} options={options} value={limit} />
+			</span>
 		</>
 	)
 }
